9c9bb92bcd838e99191697a1649eef1bc6b9fb99
dilipgp <45794342+dilipgp@users.noreply.github.com>
2024-07-19 16:53:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.113.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
