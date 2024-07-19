07676bf0799181bc6ced409d27b697e2c22133f2
Dilip Singh <dilip.singh@coxautoinc.com>
2024-07-19 17:33:44 +0530

    Updated code

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   backend "azurerm" {                             # Use AzureRM backend for storing Terraform state
     resource_group_name = "POC01"                 # Azure Resource Group where Terraform state is stored
     storage_account_name = "avdfsst"   # Name of the Azure Storage Account for Terraform state
-    container_name = "statefile"                  # Name of the Azure Storage Container for Terraform state
+    container_name = "dev-statefile"                  # Name of the Azure Storage Container for Terraform state
     key = "terraform.tfstate"                     # Key or path to the Terraform state file within the container
   }
 }
