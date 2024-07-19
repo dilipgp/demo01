7e2156edc967144eaa8da0f4e0fda5ab363ee606
Dilip Singh <dilip.singh@coxautoinc.com>
2024-07-19 17:21:12 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -1,26 +1,13 @@
-#Remote state file 
-terraform {
-  backend "azurerm" {
-    resource_group_name = "POC01"
-    storage_account_name = "avdfsst"
-    container_name = "statefile"
-    key = "terraform.tfstate"
-  }
-}
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  #skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-features {}
+  features {}
+}
+ 
+# Define Terraform backend configuration
+terraform {
+  backend "azurerm" {                             # Use AzureRM backend for storing Terraform state
+    resource_group_name = "POC01"                 # Azure Resource Group where Terraform state is stored
+    storage_account_name = "avdfsst"   # Name of the Azure Storage Account for Terraform state
+    container_name = "statefile"                  # Name of the Azure Storage Container for Terraform state
+    key = "terraform.tfstate"                     # Key or path to the Terraform state file within the container
+  }
 }
